24c3dde7c069616b6adb69e164ab58707b44c070
Rua-A <rootxbot@gmail.com>
2023-05-19 16:25:04 +0900

    feat: add exit menu
    imput 10 on menupage

--- 파이썬_미니_프로젝트/계산기.py
+++ 파이썬_미니_프로젝트/계산기.py
@@ -1,30 +1,32 @@
 #간단한 계산기:
+def main(): 
 #a. 사용자에게 두 개의 숫자를 입력하라는 메시지를 표시합니다.
-while True:
-     try:
-          f = float(input('첫번째 숫자를 입력하세요.'))
-          break  
-     except ValueError:  
-          print("숫자를 입력해주세요.")
-while True:
-     try:
-          s = float(input('두번째 숫자를 입력하세요.'))
-          break  
-     except ValueError:  
-          print("숫자를 입력해주세요.")
-print(f'첫번째 수 : {f}, 두번째 수 : {s}')
+     while True:
+          try:
+               f = float(input('첫번째 숫자를 입력하세요.'))
+               break  
+          except ValueError:  
+               print("숫자를 입력해주세요.")
+     while True:
+          try:
+               s = float(input('두번째 숫자를 입력하세요.'))
+               break  
+          except ValueError:  
+               print("숫자를 입력해주세요.")
+     print(f'첫번째 수 : {f}, 두번째 수 : {s}')
 
-#b. 산술 연산(예: 더하기, 빼기, 곱하기, 나누기)이 있는 메뉴를 표시합니다.
-while True:
-     try:
-          ch = int(input('진행할 연산을 고르세요. 1.더하기 , 2.빼기, 3.곱하기, 4.나누기, 5.지수, 6.모듈러스\n'))
-          if ch < 1 or ch > 6:
-               raise ValueError("1부터 4까지의 번호를 입력해주세요.")
-          break
-     except ValueError as e:
-          print(e)
+     #b. 산술 연산(예: 더하기, 빼기, 곱하기, 나누기)이 있는 메뉴를 표시합니다.
+     while True:
+          try:
+               ch = int(input('진행할 연산을 고르세요. 1.더하기 , 2.빼기, 3.곱하기, 4.나누기, 5.지수, 6.모듈러스\n'))
+               if ch < 1 or ch > 6 and ch != 10:
+                    raise ValueError("1부터 4까지의 번호를 입력해주세요.")
+               break
+          except ValueError as e:
+               print(e)
+     return f, s, ch
+
 #c. 사용자의 선택에 따라 숫자에 대해 선택한 작업을 수행합니다.
-
 def one_Addition(f,s):
      print(f'더하기 {f} + {s} ')
      return f+s
@@ -59,4 +61,9 @@ def switch_case(x,f,s):
      else:
           return fucn()
 #d. 결과를 사용자에게 표시합니다.
-print(f'결과 : {switch_case(ch,f,s)}')  
+while True:
+     print('종료 : 메뉴 선택에서 10번을 입력하세요.')
+     f,s,ch = main()
+     if ch == 10:
+          break
+     print(f'결과 : {switch_case(ch,f,s)}')  
